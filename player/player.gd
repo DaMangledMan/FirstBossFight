30f47e648839afa0a797d56065c55276fdc5570a
DaMangledMan <jacobh52003@gmail.com>
2023-10-05 16:17:03 -0600

    "finished" whitebox for player. started whitebox for boss

--- player/player.gd
+++ player/player.gd
@@ -59,98 +59,153 @@ func _process(delta):
 	if dodging: # handles dodge case
 		velocity = velocity.normalized() * _speed * 2.0
 		
-		if velocity.x > 0 or last_direction == "right":
+		if velocity.x > 0:
 			$AnimatedSprite2D.animation = "dodge_right"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "right"
-		elif velocity.x < 0 or last_direction == "left":
+		elif velocity.x < 0:
 			$AnimatedSprite2D.animation = "dodge_right"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = true
 			last_direction == "left"
-		elif velocity.y < 0 or last_direction == "up":
+		elif velocity.y < 0:
 			$AnimatedSprite2D.animation = "dodge_up"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "up"
-		elif velocity.y > 0 or last_direction == "down":
+		elif velocity.y > 0:
 			$AnimatedSprite2D.animation = "dodge_down"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "down"
 	
+	
 	elif healing: # handles heal case
 		velocity = velocity.normalized() * _speed * .25
 		
-		if velocity.x > 0 or last_direction == "right":
+		if velocity.x > 0:
 			$AnimatedSprite2D.animation = "heal_right"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "right"
-		elif velocity.x < 0 or last_direction == "left":
+		elif velocity.x < 0:
 			$AnimatedSprite2D.animation = "heal_right"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = true
 			last_direction == "left"
-		elif velocity.y < 0 or last_direction == "up":
+		elif velocity.y < 0:
 			$AnimatedSprite2D.animation = "heal_up"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "up"
-		elif velocity.y > 0 or last_direction == "down":
+		elif velocity.y > 0:
 			$AnimatedSprite2D.animation = "heal_down"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "down"
+		else:
+			if last_direction == "right":
+				$AnimatedSprite2D.animation = "heal_right"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = false
+			elif last_direction == "left":
+				$AnimatedSprite2D.animation = "heal_right"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = true
+			elif last_direction == "up":
+				$AnimatedSprite2D.animation = "heal_up"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = false
+			elif last_direction == "down":
+				$AnimatedSprite2D.animation = "heal_down"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = false
+	
 	
 	elif attacking and attack_type == "light": # handles light attack case
 		velocity = velocity.normalized() * _speed * .1
 		
-		if velocity.x > 0 or last_direction == "right":
+		if velocity.x > 0:
 			$AnimatedSprite2D.animation = "light_attack_right"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "right"
-		if velocity.x < 0 or last_direction == "left":
+		elif velocity.x < 0:
 			$AnimatedSprite2D.animation = "light_attack_right"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = true
 			last_direction == "left"
-		elif velocity.y < 0 or last_direction == "up":
+		elif velocity.y < 0:
 			$AnimatedSprite2D.animation = "light_attack_up"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "up"
-		elif velocity.y > 0 or last_direction == "down":
+		elif velocity.y > 0:
 			$AnimatedSprite2D.animation = "light_attack_down"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "down"
+		else:
+			if last_direction == "right":
+				$AnimatedSprite2D.animation = "light_attack_right"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = false
+			elif last_direction == "left":
+				$AnimatedSprite2D.animation = "light_attack_right"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = true
+			elif last_direction == "up":
+				$AnimatedSprite2D.animation = "light_attack_up"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = false
+			elif last_direction == "down":
+				$AnimatedSprite2D.animation = "light_attack_down"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = false
+	
 	
 	elif attacking and attack_type == "heavy": # handles heavy attack case
 		velocity = velocity.normalized() * _speed * .1
 		
-		if velocity.x > 0 or last_direction == "right":
+		if velocity.x > 0:
 			$AnimatedSprite2D.animation = "heavy_attack_right"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "right"
-		elif velocity.x < 0 or last_direction == "left":
+		elif velocity.x < 0:
 			$AnimatedSprite2D.animation = "heavy_attack_right"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = true
 			last_direction == "left"
-		elif velocity.y < 0 or last_direction == "up":
+		elif velocity.y < 0:
 			$AnimatedSprite2D.animation = "heavy_attack_up"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "up"
-		elif velocity.y > 0 or last_direction == "down":
+		elif velocity.y > 0:
 			$AnimatedSprite2D.animation = "heavy_attack_down"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "down"
+		else:
+			if last_direction == "right":
+				$AnimatedSprite2D.animation = "heavy_attack_right"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = false
+			elif last_direction == "left":
+				$AnimatedSprite2D.animation = "heavy_attack_right"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = true
+			elif last_direction == "up":
+				$AnimatedSprite2D.animation = "heavy_attack_up"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = false
+			elif last_direction == "down":
+				$AnimatedSprite2D.animation = "heavy_attack_down"
+				$AnimatedSprite2D.flip_v = false
+				$AnimatedSprite2D.flip_h = false
+	
 	
 	else: # handles normal movement case
 		if velocity.length() > 0:
@@ -159,26 +214,27 @@ func _process(delta):
 			velocity = Vector2.ZERO
 			$AnimatedSprite2D.animation = "idle"
 		
-		if velocity.x > 0 or last_direction == "right":
+		if velocity.x > 0:
 			$AnimatedSprite2D.animation = "walk_right"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "right"
-		if velocity.x < 0 or last_direction == "left":
+		elif velocity.x < 0:
 			$AnimatedSprite2D.animation = "walk_right"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = true
 			last_direction == "left"
-		elif velocity.y < 0 or last_direction == "up":
+		elif velocity.y < 0:
 			$AnimatedSprite2D.animation = "walk_up"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "up"
-		elif velocity.y > 0 or last_direction == "down":
+		elif velocity.y > 0:
 			$AnimatedSprite2D.animation = "walk_down"
 			$AnimatedSprite2D.flip_v = false
 			$AnimatedSprite2D.flip_h = false
 			last_direction == "down"
+	
 	
 	# controls the movement on a perframe basis
 	position += velocity * delta
